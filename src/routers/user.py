050160f523463cdce981d51777a7af011c3c0cae
Andrey <ASDadaykin@gmail.com>
2025-04-15 15:14:12 +0300

    Добавлены зИсправлен файл users.py

--- src/routers/user.py
+++ src/routers/user.py
@@ -8,7 +8,7 @@ router = APIRouter()
 @router.get("", status_code=status.HTTP_200_OK, response_model=UserInfo, responses={404: {"detail": "User not found"}})
 async def get_user(email: str):
     '''Получение пользователя по email'''
-    user = db.get_user_by_email(email)`
+    user = db.get_user_by_email(email)
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return UserInfo(
